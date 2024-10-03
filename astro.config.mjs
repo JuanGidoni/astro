d2e0dd7224b19b6193b96fd8ab005b597bb85478
Juan Ignacio Gidoni <juan.gidoni@gmail.com>
2024-10-03 08:00:57 +0000

    Added config site to deploy into GPH

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,4 +8,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
+  site: "https://juangidoni.github.io",
+  base: "astro"
 });
